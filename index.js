54fcac51af48d7a395460cb8ac57bb943c4e1acd
eliecerecology <eliecerecology@gmail.com>
2019-11-09 16:45:45 +0200

    I add a graph

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-'strict'
-console.log("my App");
+'use strict';
+const express = require('express');
+const app = express();
+
+//app.get('/', function (req, res) {
+//    res.send('<h1>Welcome to my first app :-)</h1>')
+//});
+
+app.get('/demo', (req, res) =>{
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/', (req, res) =>{
+  res.sendFile(__dirname + '/chart.html')
+});
+
+app.listen(3000,() =>{
+    console.log('server listening port 3000')
+});
